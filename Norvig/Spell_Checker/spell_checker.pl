339297636e8a6d0300908f66e5c932c7b2fb46ed
Vishaal Kalwani <vishaalk@umich.edu>
2014-04-20 14:41:34 -0400

    I think I have to upgrade to Perl 6 for this to work

--- Norvig/Spell_Checker/spell_checker.pl
+++ Norvig/Spell_Checker/spell_checker.pl
@@ -11,19 +11,30 @@ sub train {
 	return %res; # fill and return empty hash
 }
 
+sub splits {
+	my $word = shift @_;
+	my @splits = ();
+	for (my $i = 0; $i < length $word; $i++) {
+		my ($first, $last) = (substr($word, 0, $i), substr($word, $i));
+		push @splits, ($first, $last);
+	}
+	print "@splits\n";
+	return @splits;
+}
 
 my @set = ("Vishaal", "April", "Aroosa", "Alex", "April", "Alex");
 my %NWORDS = train(@set);
 while ( (my $key, my $value) = each %NWORDS ) {
-	print "key: $key, value: $value\n";
+	#print "key: $key, value: $value\n";
 }
 
 my @alphabet = 'a' .. 'z';
 my $word = "spelling";
-
 # basic work for splits function 
 for (my $i = 1; $i < length $word; $i++) {
 	my ($first, $last) = (substr($word, 0 , $i), substr($word, $i));
 	#print substr($word, 0, $i) . " " . substr($word, $i) . "\n";
-	print $first . " " . $last . "\n";
+	#print $first . " " . $last . "\n";
 }
+
+my @splits = splits($word);
