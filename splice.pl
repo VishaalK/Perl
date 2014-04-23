04c35389b6e74123a5259c9df1a24e5a38024df5
Vishaal Kalwani <vishaalk@umich.edu>
2014-04-23 17:29:18 -0400

    Fixed the file layout

--- splice.pl
+++
@@ -1,9 +0,0 @@
-use strict;
-use warnings;
-
-my @array = ("Vishaal", "Peter", "Chris", "Spencer");
-print @array; print "\n";
-
-# does (start, end] in the splice
-splice(@array, 1, 3, "nerd", "geek");
-print "@array \n";
