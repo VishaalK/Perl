9229cdfaa02c2a7ffb0f4e9010c8e51572c0dddd
Vishaal Kalwani <vishaalk@umich.edu>
2014-04-23 14:49:59 -0400

    Did some anonymous functions stuff

--- Tutorials/array.pl
+++ Tutorials/array.pl
@@ -2,7 +2,7 @@ use strict;
 use warnings;
 
 my @friends = ("Vishaal", "April", "Aroosa", "Ashley", "Megan", "Nicole");
-print join(" - " , @friends) . "\n";
+print "ORIGINAL: " . join(", " , @friends) . "\n";
 
 # (scalar reverse) concatenates the list and reverses it
 # otherwise just revses the list
@@ -11,10 +11,10 @@ print join(" - " , @friends) . "\n";
 print reverse(@friends) . "\n";
 
 # print "\n list reverse join(\" - \", \@friends) \n";
-print reverse(join(" - ", @friends)) . "\n";
+print reverse(join(", ", @friends)) . "\n";
 
 # print "\n scalar reverse join(\" - \", \@friends) \n"; 
-print scalar reverse (join(" - ", @friends)) . "\n";
+print scalar reverse (join(", ", @friends)) . "\n";
 
 # print "\n map \n";
 print join(", ", map { lc $_ } @friends) . "\n";
@@ -22,4 +22,6 @@ print join(", ", map { lc $_ } @friends) . "\n";
 my @classes = (482, 485, 565, 347);
 print reverse (@classes) . "\n";
 
+print "\nPassing anonymous functions on arrays\n"; 
 print join(", ", @classes) . "\n";
+print join(", ", grep { $_ >= 400 } @classes) . "\n";
